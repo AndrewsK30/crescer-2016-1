0d450f7d512f604185f1b0dc85ae1f333fb8c4a0
Andrews kuhn <andrewskuhn@hotmail.com>
2016-04-06 12:21:10 -0300

    feat (funcionalidades) - adicionando classe Dwarf

---
+++ src/modulo-01-java-OO/projeto-lotr-bluej/Dwarf.java
@@ -0,0 +1,9 @@
+public class Dwarf {
+    private String nome;
+    public int vida=110;
+    public Dwarf(String nome){
+        // inicializa variáveis de instância
+       this.nome = nome;       
+    }    
+     
+ }
