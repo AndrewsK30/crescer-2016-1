054066ec40c34ba5b2ef37bb9627214148cb9f49
Andrews Kuhn da Silva <andrews.silva@cwinet.local>
2016-04-06 14:48:58 -0300

    fix(Elfo) - Retirada de numero de elfos, desnecessario para o contexto

--- src/modulo-01-java-OO/projeto-lotr-bluej/Elfo.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/Elfo.java
@@ -1,17 +1,14 @@
 public class Elfo {
     private String nome;
     private int experiencia=0, flechas = 42;
-    public static int numElfos=0;
     public Elfo(String nome, int flechas){
         // inicializa variáveis de instância
        this.nome = nome;
        this.flechas=flechas;
-       numElfos++;
     }    
      //Exercício 3-E a resposta para tudo é...
     public Elfo(String nome){
        this.nome = nome;
-       numElfos++;
     }
     
     public void atirarFlecha(Dwarf Obj) {
