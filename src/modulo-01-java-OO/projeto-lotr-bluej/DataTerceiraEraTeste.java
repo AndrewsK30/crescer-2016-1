2112bca23f364cbaf05a9d7b4ce012a4689542f4
Andrews kuhn <andrewskuhn@hotmail.com>
2016-04-07 10:36:07 -0300

    test(DataTerceiraEra) - adionado novos testes.

--- src/modulo-01-java-OO/projeto-lotr-bluej/DataTerceiraEraTeste.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/DataTerceiraEraTeste.java
@@ -3,7 +3,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-class DataTerceiraEraTeste {
+public  class DataTerceiraEraTeste {
 	@Test
 	public void EsteAnoDeveSerBissexto(){
 		DataTerceiraEra data = new DataTerceiraEra(12,5,2016);
@@ -15,5 +15,20 @@ class DataTerceiraEraTeste {
 		assertEquals(7,data.getDia());
 		assertEquals(4,data.getMes());
 		assertEquals(2016,data.getAno());
+	}
+	@Test
+	public void Ano2100DeveSerBissexto(){
+		DataTerceiraEra data = new DataTerceiraEra(12,5,2100);
+		assertEquals(false,data.ehBissexto());
+	}
+	@Test
+	public void Ano2002NaoDeveSerBissexto(){
+		DataTerceiraEra data = new DataTerceiraEra(12,5,2002);
+		assertEquals(false,data.ehBissexto());
+	}
+	@Test
+	public void Ano416DeveSerBissexto(){
+		DataTerceiraEra data = new DataTerceiraEra(12,5,416);
+		assertEquals(true,data.ehBissexto());
 	}
 }
