f1b0ff5895e88e44241a56db6ee6b57437ddc9c8
Andrews Kuhn da Silva <andrews.silva@cwinet.local>
2016-04-07 15:49:34 -0300

    feat(Dwarf) - adicinado ao metodo de perdervida, uma verificação para ver se o dwarf esta vivo.

--- src/modulo-01-java-OO/projeto-lotr-bluej/Dwarf.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/Dwarf.java
@@ -1,18 +1,24 @@
 public class Dwarf {
     private String nome;
     private int vida=110;
+    private Status status = Status.VIVO;
    public Dwarf(String nome){   
         this.vida=110;
         this.nome = nome; 
    }         
    public void perderVida(){
-        this.vida-=10;
+       this.vida-=10;
+       if(vida==0)
+        this.status=Status.MORTO;    
    }
    public void setName(String nome){
         this.nome = nome;  
    }
    public String getNome(){
        return nome;
+   }
+    public Status getStatus(){
+       return status;
    }
    public int getVida(){
        return vida;
