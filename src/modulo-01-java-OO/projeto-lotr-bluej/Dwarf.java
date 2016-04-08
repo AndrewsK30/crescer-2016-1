e676b37d9585a6311b2614a73c1e07062ebaa3fb
Andrews Kuhn da Silva <andrews.silva@cwinet.local>
2016-04-08 15:04:32 -0300

    feat(Dwarf) - adiconado perder o item com ganhar, e retorna inventario. fix(Dwarf) - arrumado get numero para nomes nulos.

--- src/modulo-01-java-OO/projeto-lotr-bluej/Dwarf.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/Dwarf.java
@@ -1,21 +1,26 @@
+import java.util.*;
 public class Dwarf {
    private String nome;
    private int vida=110;
    private int experiencia=0;
    private Status status = Status.VIVO;
    private DataTerceiraEra dataNascimento = new DataTerceiraEra(1,1,1);
+   private Inventario inventario = new Inventario();
+   
    public Dwarf(String nome, DataTerceiraEra dataNascimento){   
-        this.nome = nome;
+        this(nome);
         this.dataNascimento = dataNascimento;
    } 
+   
    public Dwarf(String nome){   
         this.nome = nome;  
    } 
+   
    public void perderVida(){
        double numeroSorte=this.getNumeroSorte();
-       if( numeroSorte < 0 ){
-           experiencia+=2;
-       }else if(numeroSorte >= 0.0 && !(numeroSorte <= 100.0)){
+       if(numeroSorte < 0){
+           experiencia += 2;
+       }else if(numeroSorte > 100){
            int vidaAposFlechada= this.vida - 10;
            if(vida == 0){
                status = Status.MORTO;
@@ -27,26 +32,49 @@ public class Dwarf {
       
       
    }
+   
    public double getNumeroSorte(){
        double retorno = 101.0;
        if(dataNascimento.ehBissexto() && vida >= 80 && vida <= 90 ){
             retorno*=-33;
-       }else if(!(dataNascimento.ehBissexto())&& (nome == "Seixas" || nome == "Meireles")){
-           retorno=(retorno*33)%100;
+       }else if(!dataNascimento.ehBissexto() &&
+       this.nome!= null && 
+       (this.nome.equals("Seixas") || this.nome.equals("Meireles"))){
+           retorno = ( retorno * 33 ) % 100;
        }
        return retorno;
    }
+   
    public void setName(String nome){
         this.nome = nome;  
    }
+   
    public String getNome(){
        return nome;
    }
-    public Status getStatus(){
+   
+   public Status getStatus(){
        return status;
    }
+   
+   public void adicionarItem(Item item){
+       inventario.adicionarItem(item);
+   }
+   
+   public void perderItem(Item item){
+       inventario.removerItem(item);
+   }
+   
+   public Inventario getInventario(){
+       return inventario;
+   }
+   
    public int getVida(){
        return vida;
+   }
+   
+   public DataTerceiraEra getDataNascimento(){
+       return dataNascimento;
    }
    public int getExp(){
        return experiencia;
