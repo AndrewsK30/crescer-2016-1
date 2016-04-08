95fe8e33a9ecef8a9bd12be14873fe16fa5d8ac1
Andrews kuhn <andrewskuhn@hotmail.com>
2016-04-08 01:05:37 -0300

    feat(Dwarf) -  adicionado numero sorte e novas implematações no perdervida

--- src/modulo-01-java-OO/projeto-lotr-bluej/Dwarf.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/Dwarf.java
@@ -1,6 +1,7 @@
 public class Dwarf {
     private String nome;
     private int vida=110;
+    private int experiencia=0;
     private Status status = Status.VIVO;
     private DataTerceiraEra dataNascimento;
    public Dwarf(String nome, DataTerceiraEra dataNascimento){   
@@ -14,13 +15,29 @@ public class Dwarf {
         this.dataNascimento = new DataTerceiraEra(1,1,1);
    } 
    public void perderVida(){
-       int vidaAposFlechada= this.vida - 10;
-       if(vida == 0){
-           status = Status.MORTO;
-       }       
-       if (vida > 0){
-           vida = vidaAposFlechada;
+       double numeroSorte=this.getNumeroSorte();
+       if( numeroSorte < 0 ){
+           experiencia+=2;
+       }else if(numeroSorte >= 0.0 && !(numeroSorte <= 100.0)){
+           int vidaAposFlechada= this.vida - 10;
+           if(vida == 0){
+               status = Status.MORTO;
+           }       
+           if (vida > 0){
+               vida = vidaAposFlechada;
+           }
        }
+      
+      
+   }
+   public double getNumeroSorte(){
+       double retorno = 101.0;
+       if(dataNascimento.ehBissexto() && vida >= 80 && vida <= 90 ){
+            retorno*=-33;
+       }else if(!(dataNascimento.ehBissexto())&& (nome == "Seixas" || nome == "Meireles")){
+           retorno=(retorno*33)%100;
+       }
+       return retorno;
    }
    public void setName(String nome){
         this.nome = nome;  
@@ -33,5 +50,8 @@ public class Dwarf {
    }
    public int getVida(){
        return vida;
-    }
+   }
+   public int getExp(){
+       return experiencia;
+   }
  }
