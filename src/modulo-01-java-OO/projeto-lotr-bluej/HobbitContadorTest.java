1ce5e7c9ba3c291f545cd8bd98979e18f6b0d772
Andrews kuhn <andrewskuhn@hotmail.com>
2016-04-09 15:21:21 -0300

    test(HobbitContador) - adicionado testes.

--- src/modulo-01-java-OO/projeto-lotr-bluej/HobbitContadorTest.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/HobbitContadorTest.java
@@ -20,10 +20,33 @@ public class HobbitContadorTest
        arrayDePares.add(new ArrayList<>(Arrays.asList(15, 18)));
        arrayDePares.add(new ArrayList<>(Arrays.asList(4, 5)));
        arrayDePares.add(new ArrayList<>(Arrays.asList(12, 60)));
-        arrayDePares.add(new ArrayList<>(Arrays.asList(31, 33)));
+       arrayDePares.add(new ArrayList<>(Arrays.asList(31, 33)));
        arrayDePares.add(new ArrayList<>(Arrays.asList(3, 4)));
        arrayDePares.add(new ArrayList<>(Arrays.asList(4, 64)));  
        HobbitContador contador = new HobbitContador();
        assertEquals(1032,contador.calcularDiferenca(arrayDePares));
    }
+   @Test
+   public void DiferencaParesVazio() {
+       ArrayList<ArrayList<Integer>> arrayDePares = new ArrayList<>();
+       HobbitContador contador = new HobbitContador();
+       assertEquals(0, contador.calcularDiferenca(arrayDePares));
+   }
+   @Test
+   public void semDiferenca() {
+        ArrayList<ArrayList<Integer>> arrayDePares = new ArrayList<>();
+        arrayDePares.add(new ArrayList<>(Arrays.asList(11, 19)));
+        arrayDePares.add(new ArrayList<>(Arrays.asList(99, 37)));
+        arrayDePares.add(new ArrayList<>(Arrays.asList(4, 5))); 
+        HobbitContador contador = new HobbitContador();
+        assertEquals(0, contador.calcularDiferenca(arrayDePares));
+   }    
+   @Test
+   public void calcularDiferencaComZeroEUm() {
+       ArrayList<ArrayList<Integer>> arrayDePares = new ArrayList<>();
+       arrayDePares.add(new ArrayList<>(Arrays.asList(1, 1)));
+       arrayDePares.add(new ArrayList<>(Arrays.asList(0, 0))); 
+       HobbitContador contador = new HobbitContador();
+       assertEquals(0, contador.calcularDiferenca(arrayDePares));
+    }     
 }
