69bb1c1e14f8b00d7fa1d1dfad3660efa7c8e027
Andrews kuhn <andrewskuhn@hotmail.com>
2016-04-09 01:12:11 -0300

    feat(Hobbitcontador) - adionado classe hobbitcontador e metodo calculadiferenca.

---
+++ src/modulo-01-java-OO/projeto-lotr-bluej/HobbitContador.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class HobbitContador{
+  public int calcularDiferenca (ArrayList<ArrayList<Integer>>  array){
+      int res=0,mmc,a,b;
+      for(ArrayList<Integer> auxArray : array){
+          a=mmc=auxArray.get(0);
+          b=auxArray.get(1);
+          while ((mmc%a!=0)||(mmc%b!=0)) mmc++;
+          res += a*b-mmc;
+      }
+      return res;
+  }
+}
