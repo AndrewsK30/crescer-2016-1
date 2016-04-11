5de890dc86d4f954ba6ffd17ef863c743e9afe97
Andrews kuhn <andrewskuhn@hotmail.com>
2016-04-11 02:29:42 -0300

    fix(dwarf) - corrigido indentação.

--- src/modulo-01-java-OO/projeto-lotr-bluej/HobbitContador.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/HobbitContador.java
@@ -3,8 +3,8 @@ public class HobbitContador{
   public int calcularDiferenca (ArrayList<ArrayList<Integer>>  array){
       int res=0,mmc,a,b;
       for(ArrayList<Integer> auxArray : array){
-          a=mmc=auxArray.get(0);
-          b=auxArray.get(1);
+        a=mmc=auxArray.get(0);
+        b=auxArray.get(1);
         if(!(a==0|| b==0)){
           while ((mmc%a!=0)||(mmc%b!=0)) mmc++;
           res += a*b-mmc;
