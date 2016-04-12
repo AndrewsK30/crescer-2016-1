7840462cb6a01ca6d7f7c83d6e5cc13f6ba4cafb
Andrews Kuhn da Silva <andrews.silva@cwinet.local>
2016-04-12 14:27:53 -0300

    fix(ElfoNoturno) - corrigido metodo perder vida ao atirar flecha.

--- src/modulo-01-java-OO/projeto-lotr-bluej/ElfoNoturno.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/ElfoNoturno.java
@@ -1,25 +1,15 @@
 public class ElfoNoturno extends Elfo{
-    
-    public ElfoNoturno (String nome, int flechas){        
-       super(nome ,flechas);          
-    }    
-     
-    public ElfoNoturno(String nome){
-       super(nome);          
-    }    
+   public ElfoNoturno(String nome){
+      super(nome);          
+   }    
    /*Caso o elfo perca 5% da vida ele nunca morrera, por exemplo se ele tiver 1 de vida ele perdera
     * 0.05 e dps 5% de 0.95 e assim por diante.
     * 
-    */
-    public void atirarFlecha(Dwarf dwarf) {
-        this.experiencia+=3;
-        this.flechas--;
-        this.perdeVidaFlechada();
-        dwarf.perderVida();
-    }
-    
-    private void perdeVidaFlechada(){
-        this.vida -= vida*0.05;
-        if(this.vida<1) this.status=Status.MORTO;
-    }
+    */  
+   public void atirarFlecha(Dwarf dwarf){
+       super.atirarFlecha(dwarf);
+       this.experiencia += 2; 
+       this.vida *= 0.95;
+       if(this.vida<1) this.status=Status.MORTO;
+   }
 }
--- src/modulo-01-java-OO/projeto-lotr-bluej/ElfoNoturnoTest.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/ElfoNoturnoTest.java
@@ -3,18 +3,57 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-public class ElfoNoturnoTest
-{
+public class ElfoNoturnoTest{
+   private final double DELTA = 0.0;
    @Test
    public void atirou22FlechadasEMoreu(){
-        Elfo legolas = new ElfoNoturno("Legolas",32);
+        Elfo legolas = new ElfoNoturno("Legolas");
         Dwarf dwarf = new Dwarf("dwarf");
         int i=0;        
         while(legolas.getVida()>=1){
             legolas.atirarFlecha(dwarf);
             i++;
         }
-        assertEquals(45,i);      
-        assertEquals(0,legolas.getVida());        
-   }   
+        assertEquals(90,i);      
+        assertTrue(1>legolas.getVida());        
+   }
+   @Test
+   public void umaFlecha3DeExperiencia() {
+       Elfo legolas = new ElfoNoturno("Legolas");
+       Dwarf dwarf = new Dwarf("dwarf");
+       legolas.atirarFlecha(dwarf);
+       assertEquals(3, legolas.getExp());
+   }
+   @Test
+   public void doisFlecha6DeExperiencia() {
+       Elfo legolas = new ElfoNoturno("Legolas");
+       Dwarf dwarf = new Dwarf("dwarf");
+       legolas.atirarFlecha(dwarf);
+       legolas.atirarFlecha(dwarf);
+       assertEquals(6, legolas.getExp());
+   }
+   @Test
+   public void quandoatirarFlechaPerde5DeVida() {
+       Elfo legolas = new ElfoNoturno("Legolas");
+       Dwarf dwarf = new Dwarf("dwarf");
+       legolas.atirarFlecha(dwarf);
+       assertEquals(95, legolas.getVida(), DELTA); 
+   }
+   @Test
+   public void quandoAtirarDuasFlechasPerde9ponto75DeVida() {
+       Elfo legolas = new ElfoNoturno("Legolas");
+       Dwarf dwarf = new Dwarf("dwarf");
+       legolas.atirarFlecha(dwarf);
+       legolas.atirarFlecha(dwarf);
+       assertEquals(90.25, legolas.getVida(), DELTA); 
+   }
+   @Test
+   public void quandoAtirarTresFlechasPerde14pontos() {
+       Elfo legolas = new ElfoNoturno("Legolas");
+       Dwarf dwarf = new Dwarf("dwarf");
+       legolas.atirarFlecha(dwarf);
+       legolas.atirarFlecha(dwarf);
+       legolas.atirarFlecha(dwarf);
+       assertEquals(85.7375, legolas.getVida(), DELTA); 
+    }  
 }
