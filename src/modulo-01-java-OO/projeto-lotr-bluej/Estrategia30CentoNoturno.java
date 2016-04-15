eb1115dd33c0897721749e4d9996522c5397d464
Andrews Kuhn da Silva <andrews.silva@cwinet.local>
2016-04-15 15:53:10 -0300

    refactory(Estrategia30CentoNoturno) - corrigido for desnecessário.

--- src/modulo-01-java-OO/projeto-lotr-bluej/Estrategia30CentoNoturno.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/Estrategia30CentoNoturno.java
@@ -1,31 +1,25 @@
 import java.util.*;
 public class Estrategia30CentoNoturno implements EstrategiaDeAtaque{   
-   private ArrayList<Elfo> OrdemDeAtaque = new  ArrayList<>();
+   private ArrayList<Elfo> ordemDeAtaque = new  ArrayList<>();
    public ArrayList<Elfo> getOrdemDoUltimoAtaque(){
-       return OrdemDeAtaque;
+       return ordemDeAtaque;
    }
    
    public void atacar(ArrayList<Elfo> pelotao, ArrayList<Dwarf> dwarves){
        if(dwarves.size()==0 || pelotao.size()==0)return;
-       int totalElfos = 0,
-           numDwarfs = dwarves.size(),
-           numElfosNoturnos = 0,           
+       int totalElfos = pelotao.size(),
+           numDwarfs = dwarves.size(),        
            ataquesNoturnos,
            aux=0;
        boolean ehNoturno;
        Elfo ElfoAtualDoAtaque = null;
            
-       for(Elfo elfo : pelotao){
-           if(elfo instanceof ElfoNoturno)
-               numElfosNoturnos++;           
-           totalElfos++;
-       }
        ataquesNoturnos=(int)(totalElfos*0.3*numDwarfs);
        for(int i=0;i<totalElfos;i++ ){
            ElfoAtualDoAtaque =  pelotao.get(i);
            ehNoturno=ElfoAtualDoAtaque instanceof ElfoNoturno;
            if(ehNoturno && aux == ataquesNoturnos)continue;
-           OrdemDeAtaque.add(ElfoAtualDoAtaque);
+           ordemDeAtaque.add(ElfoAtualDoAtaque);
            for(Dwarf dwarf : dwarves){
                if(ehNoturno && aux == ataquesNoturnos)break;
                ElfoAtualDoAtaque.atirarFlecha(dwarf);
