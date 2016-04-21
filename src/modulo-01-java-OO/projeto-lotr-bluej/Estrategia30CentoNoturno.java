049e6feeaef04841954bafb45f307f6032503b3e
Andrews kuhn <andrewskuhn@hotmail.com>
2016-04-21 11:25:22 -0300

    reafactory(Estrategia30CentoNorturno) - corrigido variavel aux para melhor entendimento.

--- src/modulo-01-java-OO/projeto-lotr-bluej/Estrategia30CentoNoturno.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/Estrategia30CentoNoturno.java
@@ -5,25 +5,27 @@ public class Estrategia30CentoNoturno implements EstrategiaDeAtaque{
        return ordemDeAtaque;
    }
    
-   public void atacar(ArrayList<Elfo> pelotao, ArrayList<Dwarf> dwarves){
+   public void atacar(ExercitoDeElfos exercito, ArrayList<Dwarf> dwarves){
+       exercito.agruparPorStatus();
+       ArrayList<Elfo> pelotao = exercito.buscar(Status.VIVO);
        if(dwarves.size()==0 || pelotao.size()==0)return;
        int totalElfos = pelotao.size(),
            numDwarfs = dwarves.size(),        
            ataquesNoturnos,
-           aux=0;
+           maxAtqueNoturnos=0;
        boolean ehNoturno;
-       Elfo ElfoAtualDoAtaque = null;
+       Elfo elfoAtualDoAtaque = null;
            
        ataquesNoturnos=(int)(totalElfos*0.3*numDwarfs);
        for(int i=0;i<totalElfos;i++ ){
-           ElfoAtualDoAtaque =  pelotao.get(i);
-           ehNoturno=ElfoAtualDoAtaque instanceof ElfoNoturno;
-           if(ehNoturno && aux == ataquesNoturnos)continue;
-           ordemDeAtaque.add(ElfoAtualDoAtaque);
+           elfoAtualDoAtaque =  pelotao.get(i);
+           ehNoturno=elfoAtualDoAtaque instanceof ElfoNoturno;
+           if(ehNoturno && maxAtqueNoturnos == ataquesNoturnos)continue;
+           ordemDeAtaque.add(elfoAtualDoAtaque);
            for(Dwarf dwarf : dwarves){
-               if(ehNoturno && aux == ataquesNoturnos)break;
-               ElfoAtualDoAtaque.atirarFlecha(dwarf);
-               if(ehNoturno)aux++;               
+               if(ehNoturno && maxAtqueNoturnos == ataquesNoturnos)break;
+               elfoAtualDoAtaque.atirarFlecha(dwarf);
+               if(ehNoturno)maxAtqueNoturnos++;               
            }           
        }
    }
