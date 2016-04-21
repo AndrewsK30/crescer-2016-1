da69cdf03d7b6707ed6bc5d9d33baadc614ff8dc
Andrews kuhn <andrewskuhn@hotmail.com>
2016-04-21 14:06:37 -0300

    refactory(EstrategiaVerdesPrimeiro) -  corrigido for e adiconado booblesort de elfos.

--- src/modulo-01-java-OO/projeto-lotr-bluej/EstrategiaVerdesPrimeiro.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/EstrategiaVerdesPrimeiro.java
@@ -8,22 +8,32 @@ public class EstrategiaVerdesPrimeiro implements EstrategiaDeAtaque{
    public void atacar(ExercitoDeElfos exercito, ArrayList<Dwarf> dwarves){
        exercito.agruparPorStatus();
        ArrayList<Elfo> pelotao = exercito.buscar(Status.VIVO);
-       if(dwarves.size()==0 || pelotao.size()==0)return;
-       for(Elfo elfo: pelotao){
-           if(elfo instanceof ElfoVerde){
+       int tamanhoDoPelotao= pelotao.size();
+       if(dwarves.size()==0 ||tamanhoDoPelotao==0)return;
+       ordenaAtaque(pelotao,tamanhoDoPelotao);
+       for(Elfo elfo: pelotao){           
                podeAtacar(elfo,dwarves);
-           }
-       }
-       for(Elfo elfo: pelotao){
-           if(elfo instanceof ElfoNoturno){
-               podeAtacar(elfo,dwarves);
-           }
-       }
+       }       
    }
    private void podeAtacar(Elfo elfo,ArrayList<Dwarf> dwarves){
       for(Dwarf dwarf : dwarves){
           elfo.atirarFlecha(dwarf);          
       }
       ordemDeAtaque.add(elfo);
+   }
+   private void ordenaAtaque(ArrayList<Elfo> pelotaoAhOrganizar, int tamanho){
+      boolean teveTroca = true;
+      while(teveTroca){
+          teveTroca=false;
+          for(int i=0;i<tamanho-1;i++){              
+              Elfo elfoAtual=pelotaoAhOrganizar.get(i);
+              Elfo elfoDepois=pelotaoAhOrganizar.get(i+1);
+              if (elfoAtual instanceof ElfoNoturno && elfoDepois instanceof ElfoVerde) {
+                    pelotaoAhOrganizar.set(i, elfoDepois);
+                    pelotaoAhOrganizar.set(i + 1, elfoAtual);
+                    teveTroca = true;
+              }
+          }
+       }
    }
 }
