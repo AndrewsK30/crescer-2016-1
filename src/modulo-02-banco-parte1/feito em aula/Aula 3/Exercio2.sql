1149b732002da9e82c8e70bc18138d08a5d30d80
Andrews kuhn <andrewskuhn@hotmail.com>
2016-04-21 16:40:10 -0300

    feat(BancoDeDados) - adicionado exercico 2/aula 3.

--- src/modulo-02-banco-parte1/feito em aula/Aula 3/Exercio2.sql
+++ src/modulo-02-banco-parte1/feito em aula/Aula 3/Exercio2.sql
@@ -24,7 +24,54 @@ SELECT Nome,
 	   DATEADD(year,50,DataNascimento) As DataDos50,
 	   DATENAME(weekday, DATEADD(year,50,DataNascimento)) as Dia
 FROM Associado;
+--7)Liste a quantidade de cidades por UF
+SELECT UF,Count(1) AS Total
+FROM Cidade
+GROUP BY UF
+ORDER BY Total DESC
+--8)Liste a quantidade de cidades que possuem mesmo nome e UF
+SELECT Nome, COUNT(1) AS Duplicados 
+FROM Cidade 
+GROUP BY Nome 
+HAVING (COUNT(1) > 1)
+--9)Proximo id da tabela associados
+SELECT MAX(IDAssociado)+1 AS ProximoID
+FROM Associado ;
+--10)Limpe CidadeAux e ensira somente nomes e UF não iguais
+TRUNCATE TABLE CidadeAux;
+INSERT INTO CidadeAux
+	SELECT Distinct  MIN(IDCidade),Nome,UF
+	FROM   Cidade
+	GROUP BY Nome,UF;
+--11)Coloque um * no incio do nome de cidades duplicadas
+UPDATE Cidade 
+SET Nome= CONCAT('*',Nome)  
+WHERE Nome IN (
+	SELECT Nome  
+	FROM Cidade 
+	GROUP BY Nome 
+	HAVING (COUNT(1) > 1)
+);
+--12)Selecione o nome e o sexo de associados, sendo que se for f= feminino e m=masculino
+SELECT Nome,
+	   CASE WHEN Sexo='F' then 'Feminino'
+			WHEN Sexo ='M' then 'Masculino'
+			ELSE 'Indefinido'
+	   END Sexo
+FROM Associado;
+--13)mostre nome, salario e percentual do impsoto de renda de empreagaods
+SELECT NomeEmpregado,Salario,
+	   CASE WHEN Salario<1164 then '0%'
+			WHEN Salario>=1164 AND Salario<=2326 then '15%'
+			ELSE '27,5%'
+	   END ImposoDeRenda
+FROM Empregado;
+--14)Deletar cidades duplicadas
+DELETE FROM Cidade WHERE IDCidade NOT IN(
+	SELECT MIN(IDCidade)
+	FROM Cidade 
+	GROUP BY Nome,UF); 
+--15)Adiciona regra para nao ter cidades iguais em estados.
 
---7)
-
-
+ALTER TABLE Cidade
+ADD CONSTRAINT UnicoEstadoCidade UNIQUE(Nome,UF);
