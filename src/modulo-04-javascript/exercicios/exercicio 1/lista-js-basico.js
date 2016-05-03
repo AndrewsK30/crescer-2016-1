549a1564e62018650ddd9a770ef723195fa17c7b
Andrews Kuhn da Silva <andrews.silva@cwinet.local>
2016-05-03 15:18:44 -0300

    fix(Javascript) - corrigido daisy game para aceitar undefined.

--- src/modulo-04-javascript/exercicios/exercicio 1/lista-js-basico.js
+++ src/modulo-04-javascript/exercicios/exercicio 1/lista-js-basico.js
@@ -1,48 +1,49 @@
 'use strict'
-function daisyGame(petalas){
-    return petalas%2 ? 'Love me' : 'Love me not';
+
+function daisyGame(petalas) {
+    return 'Love me' + (petalas % 2 === 0 ? ' not' : '');
 }
 
-function maiorTexto(array){
-    var maior = array[0];
-    for (var i = 1 ; i < array.length ; i++) {
-        if (maior.length < array[i].length){
+function maiorTexto(array) {
+    var maior = '';
+    for (var i = 0; i < array.length; i++) {
+        if (maior.length < array[i].length) {
             maior = array[i];
         }
     }
     return maior;
 }
 
-function imprime(array, fn){
-    if(!(typeof fn ==='function'))return;
-    array.forEach(function(item){
-         fn(item);
+function imprime(array, fn) {
+    if (!(typeof fn === 'function')) return;
+    array.forEach(function (item) {
+        fn(item);
     });
 }
 
-function adicionar(x){
-    return function(y){
-        return x+y;
-    };   
+function adicionar(x) {
+    return function (y) {
+        return x + y;
+    };
 }
 //https://www.quora.com/What-is-the-sum-of-n-terms-of-a-Fibonacci-series
 //Ultimo comentário.
 function fiboSum(n) {
-    if(n<=1) return 0|n;
-    var raizDe5 = Math.sqrt(5),
-        nMais2 = n+2,
-        dividendo = Math.pow(1+raizDe5,nMais2)-Math.pow(1-raizDe5,nMais2),
-        divisor = Math.pow(2,nMais2)*raizDe5;    
-    return  (dividendo/divisor)-1;
+    if (n <= 1) return 0 | n;
+    var raizDe5 = Math.sqrt(5)
+        , nMais2 = n + 2
+        , dividendo = Math.pow(1 + raizDe5, nMais2) - Math.pow(1 - raizDe5, nMais2)
+        , divisor = Math.pow(2, nMais2) * raizDe5;
+    return (dividendo / divisor) - 1;
 }
 
-function queroCafe(mascada,precos){
-    return precos.filter(function(valor){
-                return valor <= mascada;
-           }).sort().toString();    
+function queroCafe(mascada, precos) {
+    return precos.filter(function (valor) {
+        return valor <= mascada;
+    }).sort().toString();
 }
 
-function contarPorTipo(obj,tipo){
+function contarPorTipo(obj, tipo) {
     function qualTipo(v) {
         return v === null ? 'null' : typeof v !== 'undefined' && v.constructor === Array ? 'array' : typeof v;
     }
@@ -60,12 +61,14 @@ function contarPorTipo(obj,tipo){
      goku (variável global) recebe 'Son Goku', e a variável local gohan recebe  'Son' + undefined, e retorna para onde foi chamado.
 
 */
-var gohan = 'gohan', goku = 'Goku';
+var gohan = 'gohan'
+    , goku = 'Goku';
+
 function revelarSaiyaman() {
-  console.log(gohan);
-  goku = 'Son Goku';
-  var gohan = 'Son ' + gohan;
-  return gohan;
+    console.log(gohan);
+    goku = 'Son Goku';
+    var gohan = 'Son ' + gohan;
+    return gohan;
 }
 console.log(revelarSaiyaman());
 console.log(goku);
