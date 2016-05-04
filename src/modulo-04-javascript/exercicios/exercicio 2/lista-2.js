10d894457560a0cabf6e8a9f1254c268c16600b7
Andrews kuhn <andrewskuhn@hotmail.com>
2016-05-04 11:08:27 -0300

    feat(Javascript) - adicionado que restavam para lista 2.

--- src/modulo-04-javascript/exercicios/exercicio 2/lista-2.js
+++ src/modulo-04-javascript/exercicios/exercicio 2/lista-2.js
@@ -1,4 +1,13 @@
 'use strict'
+Number.prototype.precisaoDeDois = function () {
+    return Number(this.toFixed(2));
+}
+
+Array.prototype.filtroSemPeso = function (){
+    return this.filter(function (item) {
+        return 'pesoLb' in item
+    });
+}
 
 function obterDoadores() {
     return goldSaints.filter(function (obj) {
@@ -16,12 +25,82 @@ function obterCavaleiroComMaisGolpes() {
 }
 
 function obterMesesComMaisAniversarios() {
-    var contador = {};
+    var contador = {}
+        , meses = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho"
+
+
+            
+            , "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"]
+        , retorno = [];
     goldSaints.forEach(function (obj) {
         var mes = new Date(obj.dataNascimento).getMonth();
         contador[mes] = (contador[mes] || 0) + 1;
     })
-    return Math.max.apply(null, Object.keys(contador).map(function (campo) {
-        return obj[campo];
+    var maior = Math.max.apply(Math, Object.keys(contador).map(function (campo) {
+        return contador[campo];
     }));
+
+    Object.keys(contador).filter(function (campo) {
+        return contador[campo] === maior;
+    }).forEach(function (item) {
+        retorno.push(meses[item]);
+    });
+    return retorno;
+}
+
+function obterAlturaMedia() {
+    return (goldSaints.map(function (item) {
+        return item.alturaCm;
+    }).reduce(function (x, y) {
+        return x + y;
+    }) / (goldSaints.length * 100)).precisaoDeDois();
+}
+
+function obterAlturaMediana() {
+    return (function () {
+        var metade = Math.floor(goldSaints.length / 2)
+            , arrayOrdenado = goldSaints.map(function (item) {
+                return item.alturaCm;
+            }).sort(function (x, y) {
+                return x - y;
+            });
+        return (arrayOrdenado.length % 2 ? arrayOrdenado[metade] :
+            (arrayOrdenado[metade - 1] + arrayOrdenado[metade]) / 2.0) / 100;
+    })().precisaoDeDois();
+}
+
+function obterPesoMedio() {
+    var arrayOrdenadoFiltrado = goldSaints.filtroSemPeso().map(function (item) {
+        return item.pesoLb;
+    });
+
+    return ((arrayOrdenadoFiltrado.reduce(function (x, y) {
+        return x + y;
+    }) / arrayOrdenadoFiltrado.length) * 0.4536).precisaoDeDois();
+}
+
+function obterPesoMedioDoadores() {
+    var arrayOrdenadoFiltrado = obterDoadores().filtroSemPeso().map(function (item) {
+        return item.pesoLb;
+    })
+
+
+    return ((arrayOrdenadoFiltrado.reduce(function (x, y) {
+        return x + y;
+    }) / arrayOrdenadoFiltrado.length) * 0.4536).precisaoDeDois();
+}
+
+function obterIMC() {
+    var listaDeImc = [];
+    goldSaints.filtroSemPeso().forEach(function (item) {
+        listaDeImc.push(((item.pesoLb * 0.4536) / Math.pow(item.alturaCm * 0.01, 2)).precisaoDeDois());
+    });
+    return listaDeImc;
+}
+
+function obterSobrepeso() {
+    var arrayIMC = obterIMC();
+    return goldSaints.filtroSemPeso().filter(function (item,index) {
+                return arrayIMC[index] >= 25 && arrayIMC[index] <= 29.99;
+           });
 }
