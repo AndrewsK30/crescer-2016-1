86012751c48103bbfb488cc982f5751388b78d2a
Andrews kuhn <andrewskuhn@hotmail.com>
2016-05-08 14:52:22 -0300

    feat(MegaMan) - Adicionado classe ProtoMan

---
+++ src/modulo-05-C#/exercicio 1/MegaMan/MegaMan/ProtoMan.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MegaManWorld
+{
+    public class ProtoMan : Robo
+    {
+        Boolean MorreuPrimeiraVez = true;
+
+        public ProtoMan() : base("Proto Man")
+        {
+            this.Defesa = 2;
+        }
+
+        protected override int Defesa
+        {
+            get
+            {
+                return 2;
+            }
+        }
+
+        public override void ReceberAtaque(int ataque)
+        {
+            int dano = ataque - this.Defesa;
+            this.Vida -= dano;
+            if (this.Vida <= 0 && MorreuPrimeiraVez)
+            {
+                this.Vida = 20;
+                this.Ataque = 7;
+                MorreuPrimeiraVez = false;                
+            }
+        }
+    }
+}
