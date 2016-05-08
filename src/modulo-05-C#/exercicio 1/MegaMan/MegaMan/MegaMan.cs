8369c88f666e94246e3d1768e44ebb6517a29287
Andrews kuhn <andrewskuhn@hotmail.com>
2016-05-08 14:52:34 -0300

    feat(MegaMan) - Adicionado classe MegaMan

---
+++ src/modulo-05-C#/exercicio 1/MegaMan/MegaMan/MegaMan.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace MegaManWorld
+{
+    public class MegaMan : Robo
+    {
+        public MegaMan() : base("Mega Man")
+        {
+            this.Ataque = 6;
+        }
+
+        protected override int Ataque
+        {
+            get
+            {
+                return this.Vida <= 30 ? this.ataque + 3 : this.ataque ;
+            }
+        }
+
+    }
+}
