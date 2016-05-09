131ea0c0776611d8f50ddc97d1671b89383a1883
Andrews Kuhn da Silva <andrews.silva@cwinet.local>
2016-05-09 13:54:26 -0300

    feat(Robo) - adicionado novos metodos para robo.

---
+++ src/modulo-05-C#/exercicio 1/MegaMan/MegaMan/Rush.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MegaManWorld
+{
+    public class Rush : Robo,IUpgrade
+    {
+        public Rush(Chip chip) : base(chip)
+        {
+            this.Nome = "Rush";
+            this.Ataque = 4;
+            this.Defesa = 3;
+            this.maxEquipamentos = 2;
+        }
+
+        int IUpgrade.BonusAtaque
+        {
+            get
+            {
+                return this.ataque + this.bonusAtaque;
+            }
+        }
+
+        int IUpgrade.BonusDefesa
+        {
+            get
+            {
+                return this.defesa + this.bonusDefesa;
+            }
+        }
+
+        public override void Atacar(Robo robo)
+        {
+            if (!(robo is MegaMan)) base.Atacar(robo);
+
+        }
+    }
+}
