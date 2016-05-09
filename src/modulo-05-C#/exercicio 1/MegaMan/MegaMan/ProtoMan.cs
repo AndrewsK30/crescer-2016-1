c04a70b6aebf1a5cddbe62776bfe0d1002751d21
Andrews Kuhn da Silva <andrews.silva@cwinet.local>
2016-05-09 13:51:41 -0300

    feat(ProtoMan) - adicionado funcionalidades ate Nivel4(Falta arrumar Parte Do Chip)

---
+++ src/modulo-05-C#/exercicio 1/MegaMan/MegaMan/ProtoMan.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MegaManWorld
+{
+    public class ProtoMan : Robo
+    {
+        Boolean AindaNaoMorreu = true;
+
+        public ProtoMan(Chip chip) : base(chip)
+        {
+            this.Nome = "Proto Man";
+            this.Defesa = 2;
+        }
+
+        protected override int Ataque
+        {
+            get
+            {
+                return (AindaNaoMorreu ? this.ataque : this.ataque + 2) + bonusAtaque;
+            }
+        }
+
+        protected override void ReceberAtaque(int ataque)
+        {
+            base.ReceberAtaque(ataque);
+            if (AindaNaoMorreu && this.Vida <= 0)
+            {
+                this.Vida = 20;                
+                AindaNaoMorreu = false;                
+            }
+        }
+    }
+}
