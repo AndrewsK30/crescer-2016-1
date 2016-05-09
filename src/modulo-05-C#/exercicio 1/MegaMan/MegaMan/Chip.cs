412b863d46d197a79ebfbde743a5405179214466
Andrews Kuhn da Silva <andrews.silva@cwinet.local>
2016-05-09 13:53:13 -0300

    feat(Chip) - Adicionado chips.

---
+++ src/modulo-05-C#/exercicio 1/MegaMan/MegaMan/Chip.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MegaManWorld
+{
+    public enum Chip
+    {
+        Nivel1,
+        Nivel2,
+        Nivel3
+    }
+}
