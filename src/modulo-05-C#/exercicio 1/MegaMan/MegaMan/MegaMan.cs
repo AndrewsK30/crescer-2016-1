fdffe118587549453e8cd0d8930083403b8c71db
Andrews Kuhn da Silva <andrews.silva@cwinet.local>
2016-05-09 13:53:48 -0300

    feat(InterfarceUpgrades) - adicionado interface de itens.

---
+++ src/modulo-05-C#/exercicio 1/MegaMan/MegaMan/MegaMan.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace MegaManWorld
+{
+    public class MegaMan : Robo
+    {
+        public MegaMan() : base(chip)
+        {
+            this.Nome = "Mega Man";
+            this.Ataque = 6;
+            
+        }
+        
+        protected override int Ataque
+        {
+            get
+            {
+                return (this.Vida <= 30 ? this.ataque + 3 : this.ataque) + this.bonusAtaque;
+            }
+        }
+    }
+}
