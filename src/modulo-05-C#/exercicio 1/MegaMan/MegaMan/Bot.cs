8d9e36303a788e0fadf468b8d8408e754b7a1d14
Andrews Kuhn da Silva <andrews.silva@cwinet.local>
2016-05-09 13:57:33 -0300

    feat(Bot) - adicionado Bot.

---
+++ src/modulo-05-C#/exercicio 1/MegaMan/MegaMan/Bot.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MegaManWorld
+{
+    public class Bot : Robo
+    {
+        public Bot()
+        {
+            this.Nome = "Bot";
+        }
+    }
+}
