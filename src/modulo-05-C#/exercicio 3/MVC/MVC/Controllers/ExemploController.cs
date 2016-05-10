87ab4d4dc5141e105e776143c6cf569be530f609
Andrews Kuhn da Silva <andrews.silva@cwinet.local>
2016-05-10 17:31:57 -0300

    feat(MVC) - arquivos do exercícios.

---
+++ src/modulo-05-C#/exercicio 3/MVC/MVC/Controllers/ExemploController.cs
@@ -0,0 +1,56 @@
+﻿using MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC.Controllers
+{
+    public class ExemploController : Controller
+    {
+        public ActionResult Index(int id=1)
+        {
+            var personagens = new List<PersonagemModel>()
+                {
+                    new PersonagemModel()
+                    {
+                        Nome = "Sasuke",
+                        Id = 16,
+                        Fodao = false
+                    },
+                    new PersonagemModel()
+                    {
+                        Nome = "Vegeta",
+                        Id = 30,
+                        Fodao = true
+                    },
+                    new PersonagemModel()
+                    {
+                        Nome = "Patrik",
+                        Id = 11,
+                        Fodao = true
+                    },
+                    new PersonagemModel()
+                    {
+                        Nome = "Pepe",
+                        Id = 1,
+                        Fodao = true
+                    }
+                };
+
+            ViewBag.MensagemBoasVindas = "Olá, seja bem vindo!";
+
+         
+                var personagemEscolhido = personagens.FirstOrDefault(x => x.Id == id);
+
+                return View(personagemEscolhido);
+      
+        }
+
+        public ActionResult UsandoLayout()
+        {
+            return View();
+        }
+    }    
+}
---
+++ src/modulo-05-C#/exercicio 3/MVC/MVC/Models/SobreMimModel.cs.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC.Models
+{
+    public class SobreMimModel
+    {
+        public string Nome { get; set; }
+        public string Idade { get; set; }
+        public DateTime DataDeNascimento { get; set;}
+        public int Altura { get; set; }
+        public string LinkGitHub { get; set; }
+    }
+}
