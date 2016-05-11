79eca9fc8d654829732c6d55ace9460e0041b5c3
Andrews kuhn <andrewskuhn@hotmail.com>
2016-05-11 12:30:47 -0300

    feat(MVC) - adicionado exercício 3.b .

---
+++ src/modulo-05-C#/exercicio 3/MVC/MVC/Controllers/DesafioController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC.Controllers
+{
+    public class DesafioController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+            ViewBag.aluno = Request.QueryString.Count>0 ? Request.QueryString["aluno"].ToString() : "fabriciorissetto";
+            return View();
+        }
+    }
+}
