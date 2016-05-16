f1a8f366d7178453d057ba5d36bae7e04e7ab855
Andrews Kuhn da Silva <andrews.silva@cwinet.local>
2016-05-16 16:52:54 -0300

    feat(SegurançaeAutenticação) - adicionado ADO.

---
+++ src/modulo-05-C#/Aula 6 - Autenticação e Segurança/Repositorio/UsuarioRepositorioADO.cs
@@ -0,0 +1,47 @@
+﻿using Dominio;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace Repositorio
+{
+    public class UsuarioRepositorioADO : IUsuarioRepositorio
+    {
+        public Usuario BuscarUsuarioPorAutenticacao(string email, string senha)
+        {
+            
+            string connectionString = ConfigurationManager.ConnectionStrings["ExemploADODotNet"].ConnectionString;
+
+            using (var conexao = new SqlConnection(connectionString))
+            {
+                string sql = "SELECT TOP 1 * FROM Usuarios WHERE email=@p_email and senha=@p_senha";
+
+                var comando = new SqlCommand(sql, conexao);
+                comando.Parameters.Add(new SqlParameter("p_email", email));
+                comando.Parameters.Add(new SqlParameter("p_senha", senha));
+
+                conexao.Open();
+
+                SqlDataReader leitor = comando.ExecuteReader();
+                Usuario usuario = null;
+                if (leitor.Read())
+                {
+                    usuario = new Usuario();
+                    usuario.Id = leitor.ReadInt("id");
+                    usuario.Email = leitor["email"].ToString();
+                    usuario.Nome = leitor["nome"].ToString();
+                }            
+                    
+                
+                return usuario;
+
+
+            }
+        }
+    }
+}
