6532a08772cc19eb5910334b31ce3686674fe876
Andrews Kuhn da Silva <andrews.silva@cwinet.local>
2016-05-17 17:27:10 -0300

    Exemplo de EF.

--- src/modulo-05-C#/Aula 6 - Autenticação e Segurança/Dominio/Usuario.cs
+++ src/modulo-05-C#/Aula 6 - Autenticação e Segurança/Dominio/Usuario.cs
@@ -8,11 +8,10 @@ namespace Dominio
 {
     public class Usuario
     {
+        public int Id { get; set; }
         public string Email { get; set; }
-        public string Senha { get; set; }
-        
+        public string Senha { get; set; }        
         public string Nome { get; set; }
-
         public string[] Permissoes { get; set; }
     }
 }
---
+++ src/modulo-05-C#/Aula 6 - Autenticação e Segurança/Repositorio.EF/ContextoDeDados.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dominio;
+
+namespace Repositorio.EF
+{
+    class ContextoDeDados : DbContext
+    {
+        public ContextoDeDados() : base("ExemploADODotNet")
+        {
+
+        }
+
+        public DbSet<Usuario> Usuario { get; set; }
+
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
