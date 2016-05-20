e33a9b8280bcf863e0f5d8c6b5d8a0ff94fbf212
Andrews kuhn <andrewskuhn@hotmail.com>
2016-05-20 02:11:08 -0300

    feat(douchedbag) - adicionado exercicio 3.

---
+++ src/modulo-06-ajax/exercicios/1/douchedbag.js
@@ -0,0 +1,26 @@
+"use strict"
+
+$("#form-procura-artista").submit(function (e) {
+    var valorDado = $("#input-procura-artista").val();
+    if(valorDado.toLocaleLowerCase() === 'justin bieber' && Math.floor(Math.random() * 100) + 1<21){
+        listaAlbunsPorId('douchebag');
+    }else{
+    $.ajax({
+            url: 'https://api.spotify.com/v1/search'
+            , method: 'GET'
+            , data: {
+                q: valorDado
+                , type : 'artist'
+            }
+            , dataType: "json"
+            , async: true
+        })
+        .then(
+            function onSuccess(res) {
+                var id = listaAlbunsPorId(res.artists.items[0].id);
+                
+            }
+        );
+    }
+    return e.preventDefault();
+});
