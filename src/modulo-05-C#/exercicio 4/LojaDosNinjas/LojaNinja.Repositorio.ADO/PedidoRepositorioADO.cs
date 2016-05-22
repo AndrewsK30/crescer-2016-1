ca4de7d36cda8b1645bbf1bf501bf2b0fd4355ce
Andrews kuhn <andrewskuhn@hotmail.com>
2016-05-22 01:49:16 -0300

    feat(Repositorio) - adicionado repositorio ADO.

---
+++ src/modulo-05-C#/exercicio 4/LojaDosNinjas/LojaNinja.Repositorio.ADO/PedidoRepositorioADO.cs
@@ -0,0 +1,195 @@
+﻿using LojaNinja.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LojaNinja.Repositorio.ADO
+{
+  
+    public class PedidoRepositorioADO : RepositorioBase, IPedidoRepositorio
+    {
+        public void AtualizarPedido(Pedido pedido)
+        {
+            using (var conexao = new SqlConnection(ConnectionString))
+            {
+                string sql =
+                    @"UPDATE [dbo].[Pedido]
+                        SET [DataPedido] = @p_dataPedido
+                            ,[DataEntregaDesejada] = @p_dataEntregaDesejada
+                            ,[NomeProduto] = @p_nomeProduto
+                            ,[Valor] = @p_valor
+                            ,[TipoPagamento] = @p_tipoPagamento
+                            ,[NomeCliente] = @p_nomeCliente
+                            ,[Cidade] = @p_cidade
+                            ,[Estado] = @p_estado
+                            ,[PedidoUrgente] = @p_pedidoUrgente
+                        WHERE Id = @p_id";
+
+                var comando = new SqlCommand(sql, conexao);
+                comando.Parameters.Add(new SqlParameter("p_dataPedido", pedido.DataPedido));
+                comando.Parameters.Add(new SqlParameter("p_dataEntregaDesejada", pedido.DataEntregaDesejada));
+                comando.Parameters.Add(new SqlParameter("p_nomeProduto", pedido.NomeProduto));
+                comando.Parameters.Add(new SqlParameter("p_valor", pedido.Valor));
+                comando.Parameters.Add(new SqlParameter("p_tipoPagamento", pedido.TipoDePagamento));
+                comando.Parameters.Add(new SqlParameter("p_nomeCliente", pedido.NomeCliente));
+                comando.Parameters.Add(new SqlParameter("p_cidade", pedido.Cidade));
+                comando.Parameters.Add(new SqlParameter("p_estado", pedido.Estado));
+                comando.Parameters.Add(new SqlParameter("p_pedidoUrgente", pedido.PedidoUrgente));
+                comando.Parameters.Add(new SqlParameter("p_id", pedido.Id));
+
+                conexao.Open();
+                comando.ExecuteNonQuery();
+            }
+        }
+
+        public void ExcluirPedido(int id)
+        {
+            using (var conexao = new SqlConnection(ConnectionString))
+            {
+                string sql = "DELETE FROM Pedido WHERE Id = @p_id";
+                var comando = new SqlCommand(sql, conexao);
+                comando.Parameters.Add(new SqlParameter("p_id", id));
+
+                conexao.Open();
+                comando.ExecuteNonQuery();
+            }
+        }
+
+        public void IncluirPedido(Pedido pedido)
+        {
+            using (var conexao = new SqlConnection(ConnectionString))
+            {
+                string sql =
+                    @"INSERT INTO [dbo].[Pedido]
+                        ([DataPedido]
+                        ,[DataEntregaDesejada]
+                        ,[NomeProduto]
+                        ,[Valor]
+                        ,[TipoPagamento]
+                        ,[NomeCliente]
+                        ,[Cidade]
+                        ,[Estado]
+                        ,[PedidoUrgente])
+                    VALUES
+                        (@p_dataPedido
+                        ,@p_dataEntregaDesejada
+                        ,@p_nomeProduto
+                        ,@p_valor
+                        ,@p_tipoPagamento
+                        ,@p_nomeCliente
+                        ,@p_cidade
+                        ,@p_estado
+                        ,@p_pedidoUrgente)";
+
+                var comando = new SqlCommand(sql, conexao);
+                comando.Parameters.Add(new SqlParameter("p_dataPedido", pedido.DataPedido));
+                comando.Parameters.Add(new SqlParameter("p_dataEntregaDesejada", pedido.DataEntregaDesejada));
+                comando.Parameters.Add(new SqlParameter("p_nomeProduto", pedido.NomeProduto));
+                comando.Parameters.Add(new SqlParameter("p_valor", pedido.Valor));
+                comando.Parameters.Add(new SqlParameter("p_tipoPagamento", pedido.TipoDePagamento));
+                comando.Parameters.Add(new SqlParameter("p_nomeCliente", pedido.NomeCliente));
+                comando.Parameters.Add(new SqlParameter("p_cidade", pedido.Cidade));
+                comando.Parameters.Add(new SqlParameter("p_estado", pedido.Estado));
+                comando.Parameters.Add(new SqlParameter("p_pedidoUrgente", pedido.PedidoUrgente));
+
+                conexao.Open();
+                comando.ExecuteNonQuery();
+            }
+        }
+
+        public List<Pedido> ObterPedidoPorFiltro(string cliente, string produto)
+        {
+            using (var conexao = new SqlConnection(ConnectionString))
+            {
+                SqlCommand comando = conexao.CreateCommand(); // <-- cria um SqlCommand usando a conexao.
+
+                /*
+                    * Utilizamos este 1 = 1 quando vamos concatenar
+                    * parâmetros de forma condicional.
+                    */
+                string sql = " SELECT * FROM Pedido WHERE 1 = 1 ";
+
+                if (!String.IsNullOrWhiteSpace(cliente))
+                {
+                    sql += " AND NomeCliente = @p_nomeCliente ";
+                    comando.Parameters.Add(new SqlParameter("p_nomeCliente", cliente));
+                }
+
+                if (!String.IsNullOrWhiteSpace(cliente))
+                {
+                    sql += " AND NomeProduto = @p_nomeProduto ";
+                    comando.Parameters.Add(new SqlParameter("p_nomeProduto", produto));
+                }
+
+                comando.CommandText = sql;
+
+                conexao.Open();
+
+                return LerPedidosDoBanco(comando);
+            }
+        }
+
+        public Pedido ObterPedidoPorId(int id)
+        {
+            using (var conexao = new SqlConnection(ConnectionString))
+            {
+                string sql = "SELECT * FROM Pedido WHERE Id = @p_id";
+                var comando = new SqlCommand(sql, conexao);
+                comando.Parameters.Add(new SqlParameter("p_id", id));
+
+                conexao.Open();
+
+                return LerPedidosDoBanco(comando).FirstOrDefault();
+            }
+        }
+
+        public List<Pedido> ObterPedidos()
+        {
+            using (var conexao = new SqlConnection(ConnectionString))
+            {
+                string sql = "SELECT * FROM Pedido";
+                var comando = new SqlCommand(sql, conexao);
+
+                conexao.Open();
+
+                return LerPedidosDoBanco(comando);
+            }
+        }
+
+        private List<Pedido> LerPedidosDoBanco(SqlCommand comando)
+        {
+            SqlDataReader leitor = comando.ExecuteReader();
+
+            var pedidos = new List<Pedido>();
+
+            while (leitor.Read())
+            {
+                Pedido pedido = LerPedidoDoBanco(leitor);
+                pedidos.Add(pedido);
+            }
+
+            leitor.Close();
+
+            return pedidos;
+        }
+
+        private Pedido LerPedidoDoBanco(SqlDataReader leitor)
+        {
+            return new Pedido(
+                    id: Convert.ToInt32(leitor["Id"]),
+                    dataEntregaDesejada: Convert.ToDateTime(leitor["DataEntregaDesejada"]),
+                    cidade: leitor["Cidade"].ToString(),
+                    dataPedido: Convert.ToDateTime(leitor["DataPedido"]),
+                    estado: leitor["Estado"].ToString(),
+                    nomeCliente: leitor["NomeCliente"].ToString(),
+                    nomeProduto: leitor["NomeProduto"].ToString(),
+                    pedidoUrgente: Convert.ToBoolean(leitor["PedidoUrgente"]),
+                    tipoPagamento: (TipoPagamento)Convert.ToInt32(leitor["TipoPagamento"]),
+                    valor: Convert.ToDecimal(leitor["Valor"]));
+        }
+    }
+    
+}
---
+++ src/modulo-05-C#/exercicio 4/LojaDosNinjas/LojaNinja.Repositorio.ADO/UsuarioRepositorioADO .cs
@@ -0,0 +1,166 @@
+﻿using LojaNinja.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace LojaNinja.Repositorio.ADO
+{
+    public class UsuarioRepositorioADO : RepositorioBase, IUsuarioRepositorio
+    {
+            public Usuario BuscarUsuarioPorAutenticacao(string email, string senha)
+            {
+                using (var conexao = new SqlConnection(ConnectionString))
+                {
+                    string sql = "SELECT TOP 1 * FROM Usuario WHERE Email=@p_email AND Senha=@p_senha";
+                    var comando = new SqlCommand(sql, conexao);
+                    comando.Parameters.Add(new SqlParameter("p_email", email));
+                    comando.Parameters.Add(new SqlParameter("p_senha", senha));
+
+                    conexao.Open();
+
+                    SqlDataReader leitor = comando.ExecuteReader();
+                    Usuario usuarioEncontrado = null;
+
+                    if (leitor.Read())
+                    {
+                        usuarioEncontrado = LerUsuarioDoBanco(leitor);
+                        leitor.Close();
+                    }
+
+                    if (usuarioEncontrado != null)
+                    {
+                        CarregarPermissoesDoUsuario(usuarioEncontrado, conexao);
+                    }
+
+                    return usuarioEncontrado;
+                }
+            }
+
+            public void CadastraNovoUsuario(Usuario usuario)
+            {
+            
+                using (var transaction = new TransactionScope())
+                using (var conexao = new SqlConnection(ConnectionString))
+                 {
+                    string sql =
+                        @"INSERT INTO [dbo].[Usuario]
+                                   ([Email]
+                                   ,[Senha]
+                                   ,[Nome])
+                             VALUES
+                                   (@p_email
+                                   ,@p_senha
+                                   ,@p_nome)
+                    
+                          SELECT MAX(Id) as LastId FROM [dbo].[Usuario]";
+
+                    var comando = new SqlCommand(sql, conexao);
+                    comando.Parameters.Add(new SqlParameter("p_email", usuario.Email));
+                    comando.Parameters.Add(new SqlParameter("p_senha", usuario.Senha));
+                    comando.Parameters.Add(new SqlParameter("p_nome", usuario.Nome));
+                    conexao.Open();
+                    SqlDataReader leitor = comando.ExecuteReader();
+                    leitor.Read();
+
+                    int lastId = Convert.ToInt32(leitor["LastId"]);
+
+                    leitor.Close();
+
+                    if (usuario.Permissoes != null)
+                    {
+                        foreach (Permissao permissao in usuario.Permissoes)
+                        {
+                            sql =
+                                @"INSERT INTO [dbo].[UsuarioPermissao]
+                                           ([IdUsuario]
+                                           ,[IdPermissao])
+                                     VALUES
+                                           (@p_usuario_Id
+                                           ,@p_permissao_Id)";
+
+                            comando = new SqlCommand(sql, conexao);
+                            comando.Parameters.Add(new SqlParameter("p_usuario_Id", lastId));
+                            comando.Parameters.Add(new SqlParameter("p_permissao_Id", permissao.Id));
+
+                            comando.ExecuteNonQuery();
+                        }
+                    }
+
+              
+                    transaction.Complete();
+                }
+            }
+
+            private Usuario LerUsuarioDoBanco(SqlDataReader reader)
+            {
+                return new Usuario(
+                        id: Convert.ToInt32(reader["Id"]),
+                        email: reader["Email"].ToString(),
+                        nome: reader["Nome"].ToString(),
+                        senha: reader["Senha"].ToString(),
+                        permissoes: null);
+            }
+
+            private void CarregarPermissoesDoUsuario(Usuario usuario, SqlConnection conexaoAtual)
+            {
+                var comando = new SqlCommand(
+                            @"SELECT p.* FROM Permissao p 
+                              INNER JOIN UsuarioPermissao up ON up.IdPermissao = p.Id
+                              WHERE up.IdUsuario = @p_idUsuario", conexaoAtual);
+
+                comando.Parameters.Add(new SqlParameter("p_idUsuario", usuario.Id));
+
+                SqlDataReader leitor = comando.ExecuteReader();
+
+                while (leitor.Read())
+                {
+                    Permissao permissao = LerPermissaoDoBanco(leitor);
+                    usuario.AtribuirPermissao(permissao);
+                }
+
+                leitor.Close();
+            }
+
+            private Permissao LerPermissaoDoBanco(SqlDataReader leitor)
+            {
+                return new Permissao(
+                    id: Convert.ToInt32(leitor["Id"]),
+                    nome: leitor["Nome"].ToString());
+            }
+
+            public List<Usuario> BuscarTodosUsuarios()
+            {
+                using (var conexao = new SqlConnection(ConnectionString))
+                {
+                    string sql = "SELECT Nome,Email,Permissoes FROM Usuario";
+                    var comando = new SqlCommand(sql, conexao);
+                
+                    conexao.Open();
+
+                    SqlDataReader leitor = comando.ExecuteReader();
+                    List<Usuario> usuariosList = new List<Usuario>();
+
+                    if (leitor.Read())
+                    {
+                        usuariosList.Add (LerUsuarioDoBanco(leitor));
+                        leitor.Close();
+                    }
+
+                    if (usuariosList != null)
+                    {
+                        foreach (var usuario in usuariosList)
+                        {
+                            CarregarPermissoesDoUsuario(usuario, conexao);
+                        }                    
+                    }
+
+                    return usuariosList;
+                }
+            }
+        }
+    
+}
