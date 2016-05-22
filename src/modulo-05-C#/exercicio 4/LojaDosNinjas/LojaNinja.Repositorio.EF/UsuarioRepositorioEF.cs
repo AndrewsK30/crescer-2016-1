a2d40fa710f59843f174a968cbfddbfc8c7ccb93
Andrews kuhn <andrewskuhn@hotmail.com>
2016-05-22 01:48:31 -0300

    feat(UsuarioEF) - adicionado EF usuario.

---
+++ src/modulo-05-C#/exercicio 4/LojaDosNinjas/LojaNinja.Repositorio.EF/UsuarioRepositorioEF.cs
@@ -0,0 +1,48 @@
+﻿using LojaNinja.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LojaNinja.Repositorio.EF
+{
+    public class UsuarioRepositorioEF : IUsuarioRepositorio
+    {
+        public List<Usuario> BuscarTodosUsuarios()
+        {
+            using (var db = new ContextoDeDados())
+            {
+
+                var usuario = db.Usuario.Include("Permissoes").ToList();
+
+                return usuario;
+            }
+        }
+
+        public Usuario BuscarUsuarioPorAutenticacao(string email, string senha)
+        {
+            using (var db = new ContextoDeDados())
+            {
+          
+                Usuario usuario = db.Usuario.Include("Permissoes").FirstOrDefault(
+                    u =>
+                        u.Email.Equals(email)
+                        && u.Senha.Equals(senha));
+
+                return usuario;
+            }
+        }
+
+
+        public void CadastraNovoUsuario(Usuario usuario)
+        {
+            using (var db = new ContextoDeDados())
+            {
+                db.Entry<Usuario>(usuario).State = System.Data.Entity.EntityState.Added;
+                db.SaveChanges();
+            }
+        }
+
+    }
+}
