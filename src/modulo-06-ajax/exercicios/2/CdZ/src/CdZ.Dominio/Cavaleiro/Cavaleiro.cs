ad15fafb866c2dd8fe0c4dcd0fdaf0f05f175f1f
Andrews Kuhn da Silva <andrews.silva@cwinet.local>
2016-05-23 17:31:42 -0300

    base exercicio.

--- src/modulo-06-ajax/exercicios/2/CdZ/src/CdZ.Dominio/Cavaleiro/Cavaleiro.cs
+++ src/modulo-06-ajax/exercicios/2/CdZ/src/CdZ.Dominio/Cavaleiro/Cavaleiro.cs
@@ -9,13 +9,15 @@ namespace CdZ.Dominio
         public string Nome { get; set; }
         public double AlturaCm { get; set; }
         public double PesoLb { get; set; }
-        public DateTime DataNascimento { get; private set; }
+        public DateTime DataNascimento { get; private set; }        
         public Signo Signo { get; private set; }
         public TipoSanguineo TipoSanguineo { get; private set; }
         public Local LocalNascimento { get; private set; }
         public Local LocalTreinamento { get; set; }
         public IList<Golpe> Golpes { get; private set; }
         public IList<Imagem> Imagens { get; private set; }
+
+        
 
         /*
          * Precisamos deste construtor por causa do Entity Framework.
