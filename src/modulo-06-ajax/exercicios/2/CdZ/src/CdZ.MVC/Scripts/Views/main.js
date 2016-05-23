3515a672623f88ac4c452df57d10355e7f6387da
Andrews kuhn <andrewskuhn@hotmail.com>
2016-05-23 12:36:56 -0300

    feat(main) - adicionado js para lidar com botoes.

---
+++ src/modulo-06-ajax/exercicios/2/CdZ/src/CdZ.MVC/Scripts/Views/main.js
@@ -0,0 +1,23 @@
+﻿'use strict'
+ViewCavaleiros.updateView();
+ViewCavaleiros.bindBtnCriar();
+
+setInterval(function () { ViewCavaleiros.updateView(); }, 5000);
+
+$('.anterior').click(function () {
+    ViewCavaleiros.anteriorPagina();
+});
+
+$('.proximo').click(function () {
+    ViewCavaleiros.proximaPagina();
+});
+
+$('#btn-adicionar-img').click(function () {
+    ViewCavaleiros.adicionarImageView();
+});
+
+$('#btn-adicionar-golpe').click(function () {
+    ViewCavaleiros.adicionarGolpesView();
+});
+
+
