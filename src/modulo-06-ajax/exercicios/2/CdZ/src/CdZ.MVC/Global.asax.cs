7795fedf51bad765e175fc52881ed1c6500c78a0
Andrews kuhn <andrewskuhn@hotmail.com>
2016-05-23 12:32:26 -0300

    base e dlls do exercio 2 de ajax.

---
+++ src/modulo-06-ajax/exercicios/2/CdZ/src/CdZ.MVC/Global.asax.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using CdZ.MVC.CustomModelBinders;
+
+namespace CdZ.MVC
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            ModelBinders.Binders.Add(typeof(double), new DoubleModelBinder());
+            ModelBinders.Binders.Add(typeof(double?), new DoubleModelBinder());
+        }
+    }
+}
